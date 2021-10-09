14d6a0c3ca3186b8d964786e8f538822c96147ed
a9d94t9h9 <axeldavidled999@gmail.com>
2021-10-09 20:30:49 -0300

    changes

---
+++ Screens/map/MapScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LocationSelector from '../../components/LocationSelector';
+
+const MapScreen = ()=>{
+    
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default MapScreen;
